bd992c0f276f267a789aa6ce77586604328f0664
Matt Boris <mboris@iruleathome.com>
2017-06-13 08:37:13 -0400

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularPersonalWebsitePage } from './app.po';
+
+describe('angular-personal-website App', () => {
+  let page: AngularPersonalWebsitePage;
+
+  beforeEach(() => {
+    page = new AngularPersonalWebsitePage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
