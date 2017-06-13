465c3c7e8b370d2a45192dd53724b2b4a7aa210e
Matt Boris <mboris@iruleathome.com>
2017-06-13 09:00:44 -0400

    Updated app name and title

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-personal-website App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to Matthew Boris\' Personal Website!!');
   });
 });
