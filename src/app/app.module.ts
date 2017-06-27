8a7040b440df3c9b7b125f730fee58aa688fa68c
Matt Boris <mboris@iruleathome.com>
2017-06-27 14:58:33 -0400

    Fix indenting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,21 @@ import { MaterialModule } from '@angular/material';
 import { AppComponent, DialogContentComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    MaterialModule,
-    BrowserAnimationsModule
-  ],
-  declarations: [
-    AppComponent,
-    DialogContentComponent
-  ],
-  entryComponents: [
-    DialogContentComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		MaterialModule,
+		BrowserAnimationsModule
+	],
+	declarations: [
+		AppComponent,
+		DialogContentComponent
+	],
+	entryComponents: [
+		DialogContentComponent
+	],
+	bootstrap: [
+		AppComponent
+	],
 })
 export class AppModule { }
