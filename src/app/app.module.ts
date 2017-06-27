c2454cd883369c89e99f33ef990f3d822520e498
Matt Boris <mboris@iruleathome.com>
2017-06-26 23:49:18 -0400

    Updated to Angular Demo site, will update with personal information next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import 'hammerjs';
 
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { AppComponent, DialogContentComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
   declarations: [
+    AppComponent,
+    DialogContentComponent
+  ],
+  entryComponents: [
+    DialogContentComponent
+  ],
+  bootstrap: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
